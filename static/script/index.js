609e3af9872f1336426cc4827014f27f49038e06
Meninder Purewal <meninder.purewal@gmail.com>
2021-11-30 09:47:45 -0500

    temp version

--- static/script/index.js
+++ static/script/index.js
@@ -1,28 +1,25 @@
-<script type="text/javascript">
 
-       $(function() {
+    $(document).ready(function(){
+      alert("Hello from index.js");
+    });
+
+    $(function() {
     $('#getPuns').click(function() {
         event.preventDefault();
         var form_data = new FormData($('#myform')[0]);
         console.log(form_data);
         $.ajax({
-            type: 'POST',
+            type: 'GET',
             url: 'https://bmqu6eztu2.execute-api.us-east-1.amazonaws.com/default/testEvent',
             data: form_data,
             contentType: false,
             processData: false,
         }).done(function(data, textStatus, jqXHR){
-
-            htmlCode = '<h1>Ajax Generator</h1>'
+            htmlCode = '<h1>Ajax Generator</h1>';
+            alert('htmlCode');
             $("#outputFunction").html(htmlCode);
-
-
-
         }).fail(function(data){
             alert('error!');
         });
     });
 });
-
-
-</script>
---
+++ static/script/temp.js
@@ -0,0 +1,23 @@
+
+var outputFunction = document.getElementById("outputFunction")
+var getPuns = document.getElementById("getPuns")
+
+getPuns.addEventListener("click", function(){
+        var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://bmqu6eztu2.execute-api.us-east-1.amazonaws.com/default/testEvent');
+    ourRequest.onload = function(){
+        var ourData = ourRequest.responseText;
+        console.log(ourData);
+        renderHTML(ourData)
+    };
+
+    ourRequest.send();
+
+});
+
+
+function renderHTML(data){
+    var htmlString = "";
+    htmlString += "<p>" + data[i].name + "is a " + data[i].species + "</p>";
+    outputFunction.insertAdjacentHTML('beforeend', htmlString)
+}
