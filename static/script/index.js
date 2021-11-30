0db5eacef23e0e8d18ecbae78100b92cc37dbd2e
Meninder Purewal <meninder.purewal@gmail.com>
2021-11-30 08:20:22 -0500

    first commit

---
+++ static/script/index.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+
+       $(function() {
+    $('#getPuns').click(function() {
+        event.preventDefault();
+        var form_data = new FormData($('#myform')[0]);
+        console.log(form_data);
+        $.ajax({
+            type: 'POST',
+            url: 'https://bmqu6eztu2.execute-api.us-east-1.amazonaws.com/default/testEvent',
+            data: form_data,
+            contentType: false,
+            processData: false,
+        }).done(function(data, textStatus, jqXHR){
+
+            htmlCode = '<h1>Ajax Generator</h1>'
+            $("#tableFunctions").html(htmlCode);
+
+
+
+        }).fail(function(data){
+            alert('error!');
+        });
+    });
+});
+
+
+</script>
