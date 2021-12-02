6e6690faf5373b0c5fc3c28da43afda9de91c5d8
Meninder Purewal <meninder.purewal@gmail.com>
2021-12-02 08:54:24 -0500

    testing json parse

--- static/script/index.js
+++ static/script/index.js
@@ -1,23 +1,27 @@
 
 var outputFunction = document.getElementById("outputFunction");
 var getPuns = document.getElementById("getPuns");
-var URLENDPOINT = 'https://94xn5a9ye5.execute-api.us-east-1.amazonaws.com/default/getPuns';
+//var URLENDPOINT = 'https://94xn5a9ye5.execute-api.us-east-1.amazonaws.com/default/getPuns';
+var URLENDPOINT = 'https://adhz5qf9f3.execute-api.us-east-1.amazonaws.com/default/test-12-2';
 
 getPuns.addEventListener("click", function(){
 
         var inputWord = $('#inputWord').val()
         console.log(inputWord)
 
-        urlEndpoint = URLENDPOINT + '?input_word=' + inputWord
+        //urlEndpoint = URLENDPOINT + '?input_word=' + inputWord
+        urlEndpoint = URLENDPOINT
         console.log(urlEndpoint)
 
        var punRequest = new XMLHttpRequest();
-        punRequest.open('GET', urlEndpoint);
+       punRequest.open('GET', urlEndpoint);
 
         punRequest.onload = function(){
-            var punData = punRequest.responseText;
+            var punData = JSON.parse(punRequest.responseText);
             console.log(punData);
-            renderHTML(punData)
+            //console.log(punData.0);
+            console.log(punData[0]);
+            renderHTML(punData);
             };
 
         punRequest.send();
